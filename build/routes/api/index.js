2f096a2f1a6f9eabd13bb35fdcdd68a5c3e0be8c
Tuur Meeus <tuur.meeus@student.ehb.be>
2024-08-19 01:47:36 +0200

    feat(api): created CRUD routes/functionalities to edit contents of the threads table

--- build/routes/api/index.js
+++ build/routes/api/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const usersRoute = require("./users");
+const usersRoute = require('./users');
+const threadsRoute = require('./threads')
 
 router.use('/users', usersRoute);
+router.use('/threads', threadsRoute);
 
 module.exports = router;
---
+++ build/routes/api/threads.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db')
+
+/**
+ * GET all threads saved in the database
+ * 
+ * @returns {[Object]}                   An array of all threads
+ * 
+ */
+router.get('/', function(req, resp) {
+    db.select().from('users').then(function(data) {
+        resp.send(data);
+    });
+})
+
+/**
+ * GET a thread object by their id from the database
+ * 
+ * @param   {int}         id              A number for identifying each unique thread
+ * @returns {[Object]}                    An array containing a thread object
+ */
+router.get('/:id', function(req, resp) {
+    db('users').where({id: req.params.id}).select().then(function(data) {
+        resp.send(data);
+    });
+});
+
+/**
+ * POST: create a new thread
+ * 
+ * @param   {Object}       object       An object or an array of objects containing variables for creating a new thread
+ * @param   {Object}       object.id    A number for identifying each unique thread
+ * @param   {Object}       object.user_id  A Foreign key(INT) for identifying the user that created the thread
+ * @param   {Object}       object.isResolved A boolean showing the status of the thread
+ * 
+ * @returns {Object}                    Returns an object of the created user
+ */
+router.post('/', function (req, res) {
+    db.insert(req.body).returning('*').into('users').then(function(data) {
+        res.send(data);
+    })
+});
+
+
+/**
+ * DELETE: an existing thread
+ * 
+ * @param   {int}         id              A number for identifying each unique thread
+ * @returns {[Object]}                    An array containing the deleted thread
+ */
+router.delete('/:id', function(req, res) {
+    db('users').where({id: req.params.id}).del().then(function() {
+        res.json({sucess: true});
+    });
+});
+
+module.exports = router;
